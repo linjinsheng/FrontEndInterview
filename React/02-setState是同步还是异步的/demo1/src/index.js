01fb7503ee425b819d8bb2707060d113be06cb95
“linjinsheng” <linjinshengscut@126.com>
2024-03-09 00:35:42 +0800

    修改react

--- React/02-setState是同步还是异步的/demo1/src/index.js
+++ React/02-setState是同步还是异步的/demo1/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import OnClickDemo from './OnClickDemo';
-import DomDemo from './DomDemo';
+// import DomDemo from './DomDemo';
 // import App from './App';
+import RootDemo from './RootDemo';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <OnClickDemo /> */}
-    <DomDemo />
+    {/* <DomDemo /> */}
+    <RootDemo />
     {/* <App /> */}
   </React.StrictMode>
 );
