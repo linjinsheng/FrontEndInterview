a6233c5f94583c7932c1d79467cfc816cc236f45
“linjinsheng” <linjinshengscut@126.com>
2024-04-03 00:14:31 +0800

    自定义hook

---
+++ React/22-如何自定义Hook/useAxios.js
@@ -0,0 +1,21 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+//  封装axios,发送网络请求的自定义Hook
+function useAxios(url){
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState();
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        //  利用axios发送网络请求
+        setLoading(true);
+        //  发送一个get请求
+        axios.get(url)
+             .then(res => setData(res))
+             .catch(err => setError(err))
+    }, [url]);
+    return [loading, data, error];
+}
+
+export default useAxios
