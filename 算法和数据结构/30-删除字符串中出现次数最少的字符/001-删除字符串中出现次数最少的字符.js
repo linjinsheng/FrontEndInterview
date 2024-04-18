96d367a47e4a42d8647455489c2edbb19e1054e9
“linjinsheng” <linjinshengscut@126.com>
2024-04-19 00:53:04 +0800

    修改算法

---
+++ 算法和数据结构/30-删除字符串中出现次数最少的字符/001-删除字符串中出现次数最少的字符.js
@@ -0,0 +1,40 @@
+
+var str = 'aasssccdddddd';
+var obj = {};
+var valueArr = [];
+var minArr = [];
+for(var i = 0; i < str.length; i++){
+    val = str.charAt(i);
+    if(val in obj){
+        obj[val]++;
+    }else{
+        obj[val]=1;
+    }
+}
+console.log(1, obj);
+
+for(let key in obj){
+    valueArr.push(obj[key])
+}
+console.log(2, valueArr);
+
+var minVal = Math.min(...valueArr);
+console.log(3, minVal);
+
+for(let key in obj){
+    if(obj[key] === minVal){
+      minArr.push(key)
+    }
+}
+console.log(4, minArr);
+
+function replaceStr(str, minArr){
+    for(let i = 0; i < minArr.length;i++){
+        while(str.includes(minArr[i])){
+          str = str.replace(minArr[i], "");
+        }
+    }
+    return str;
+}
+let res = replaceStr(str, minArr);
+console.log(5,res);
